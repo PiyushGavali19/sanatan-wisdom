72cbbbd06c9dd4431729e833ffc8e501493f7eac
Piyush Gavali <142135154+PiyushGavali19@users.noreply.github.com>
2026-02-09 15:10:31 +0530

    Implement routing with React Router in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
-import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
+
 import ChatComponent from './components/chatcomponent';
+import ShareChat from './pages/sharechat'; // make sure this file exists
 
 function App() {
   return (
-   <div className="flex h-screen w-screen overflow-hidden">
-      {/* Chat Area */}
-      <div className="flex-1 bg-gray-50">
-        <ChatComponent />
+    <Router>
+      <div className="flex h-screen w-screen overflow-hidden">
+
+        <div className="flex-1 bg-gray-50">
+          <Routes>
+
+            {/* Main Chat */}
+            <Route path="/" element={<ChatComponent />} />
+
+            {/* Shared Chat */}
+            <Route path="/share/:id" element={<ShareChat />} />
+
+          </Routes>
+        </div>
+
       </div>
-    </div>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
